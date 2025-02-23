2ead628ded2951e685b8242be77553d823683d1f
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-02-23 12:31:05 +0000

    m3u support

--- preload.js
+++ preload.js
@@ -1,16 +1,22 @@
-const {contextBridge, ipcRenderer} = require('electron')
+const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld('ytdlp', {
-    downloadPlaylist: (playlistURL) => ipcRenderer.invoke('downloadPlaylist', playlistURL),
-    removePlaylist: (playlistID) => ipcRenderer.invoke('removePlaylist', playlistID),
-    onPlaylistProgress: (callback) => ipcRenderer.on('playlistProgress', (_event, progress) => callback(progress)),
-    getPlaylists: () => ipcRenderer.invoke('getPlaylists'),
-    getSongs: (playlistId) => ipcRenderer.invoke('getSongs', playlistId),
-    swapPlaylists: (playlistID1, playlistID2) => ipcRenderer.invoke('swapPlaylists', playlistID1, playlistID2),
-})
+contextBridge.exposeInMainWorld("ytdlp", {
+  downloadPlaylist: (playlistURL) =>
+    ipcRenderer.invoke("downloadPlaylist", playlistURL),
+  removePlaylist: (playlistID) =>
+    ipcRenderer.invoke("removePlaylist", playlistID),
+  onPlaylistProgress: (callback) =>
+    ipcRenderer.on("playlistProgress", (_event, progress) =>
+      callback(progress),
+    ),
+  getPlaylists: () => ipcRenderer.invoke("getPlaylists"),
+  getSongs: (playlistId) => ipcRenderer.invoke("getSongs", playlistId),
+  swapPlaylists: (playlistID1, playlistID2) =>
+    ipcRenderer.invoke("swapPlaylists", playlistID1, playlistID2),
+});
 
-contextBridge.exposeInMainWorld('playback', {
-    playbackChange: (state) => ipcRenderer.send('playbackChange', state),
-    play: (callback) => ipcRenderer.on('play', (_event) => callback()),
-    pause: (callback) => ipcRenderer.on('pause', (_event) => callback()),
-})
+contextBridge.exposeInMainWorld("playback", {
+  playbackChange: (state) => ipcRenderer.send("playbackChange", state),
+  play: (callback) => ipcRenderer.on("play", (_event) => callback()),
+  pause: (callback) => ipcRenderer.on("pause", (_event) => callback()),
+});
